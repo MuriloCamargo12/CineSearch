a20826b9eb3ae8ead1d4a609db43c038340369f8
Murilo Camargo <murilo.camargo.morais@gmail.com>
2026-03-20 16:48:30 -0300

    fixed loading

--- src/app/buscar/[resultados]/page.tsx
+++ src/app/buscar/[resultados]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Loading from "@/app/loading"
 import NaoEncontrado from "@/components/NaoEncontrado"
 import ResultadoDaPesquisa from "@/components/ResultadoDaPesquisa"
 import Image from "next/image"
@@ -24,13 +25,16 @@ export default function Resultados() {
     const [resultado, setResultado] = useState<ResultProps[]>([])
     const [ativo, setAtivo] = useState(false)
     const [selecionado, setSelecionado] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function buscar() {
+            setLoading(true)
             const res = await fetch(`/api/buscar/?query=${query}`)
             const data = await res.json()
 
             setResultado(data.results)
+            setLoading(false)
         }
         buscar()
     }, [query])
@@ -43,35 +47,39 @@ export default function Resultados() {
             <section className="min-h-screen flex flex-col gap-12 py-5 bg-neutral-900 items-center pb-30">
                 <div className="w-10/12 mt-2 flex flex-col gap-2">
                     <h1 className="text-2xl font-black">{resultado.length ? 'Resultados' : ''}</h1>
-                    {/* <NaoEncontrado /> */}
-                    {dadosFiltrados.length > 0 ?
+                    {loading ? (
+                        <Loading />
+                    ) : dadosFiltrados.length > 0 ? (
                         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3">
-                            {dadosFiltrados
-                                .map(conteudo => (
-                                    <div key={conteudo.id} className="animate-fade-up animate-duration-3000 animate-delay-[1ms]">
-                                        <button className="cursor-pointer translate-y-3 transition-all duration-700 ease-out hover:translate-y-0" onClick={() => {
+                            {dadosFiltrados.map(conteudo => (
+                                <div key={conteudo.id} className="animate-fade-up animate-duration-3000 animate-delay-[1ms]">
+                                    <button
+                                        className="cursor-pointer translate-y-3 transition-all duration-700 ease-out hover:translate-y-0"
+                                        onClick={() => {
                                             setSelecionado(conteudo)
                                             setAtivo(!ativo)
-                                        }}>
+                                        }}
+                                    >
+                                        <Image
+                                            src={`https://image.tmdb.org/t/p/w500${conteudo.poster_path}`}
+                                            width={400}
+                                            height={400}
+                                            alt="banner filme/série"
+                                            className="rounded-2xl h-95 object-cover"
+                                        />
+                                    </button>
 
-                                            <Image
-                                                src={`https://image.tmdb.org/t/p/w500${conteudo.poster_path}`}
-                                                width={400}
-                                                height={400}
-                                                alt="banner filme/série"
-                                                className="rounded-2xl h-95 object-cover"
-                                            />
-                                        </button>
-                                        <div className="flex justify-center mt-3 text-center">
-                                            <h1 className="font-black">
-                                                {conteudo.name || conteudo.title}
-                                            </h1>
-                                        </div>
+                                    <div className="flex justify-center mt-3 text-center">
+                                        <h1 className="font-black">
+                                            {conteudo.name || conteudo.title}
+                                        </h1>
                                     </div>
-                                ))}
+                                </div>
+                            ))}
                         </div>
-                        : <NaoEncontrado />
-                    }
+                    ) : (
+                        <NaoEncontrado />
+                    )}
                 </div>
             </section>
         </>
