8858fe041deca7b7ae730cc7af5bc197bc690050
Murilo Camargo <murilo.camargo.morais@gmail.com>
2026-03-20 17:42:06 -0300

    Fixed loading

--- src/components/LoadingImg.tsx
+++ src/components/LoadingImg.tsx
@@ -1,6 +1,6 @@
 export default function LoadingImg() {
     return (
-        <div className="h-150 flex justify-center items-center transition-opacity duration-500 opacity-100">
+        <div className="md:h-150 h-90 flex justify-center items-center transition-opacity duration-500 opacity-100">
             <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
         </div>
     )
